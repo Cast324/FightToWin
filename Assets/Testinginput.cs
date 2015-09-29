359d97a8b36bfd91e8a1f417743936960067e651
Cast324 <joetteblades@gmail.com>
2015-09-29 06:45:21 -0500

    Updated inputs and started controller script

---
+++ Assets/Testinginput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testinginput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UserInputs ();
+	}
+
+	void UserInputs() {
+		if (Input.GetButtonDown ("360_AButton")) 
+		{
+			Debug.Log("A Button");
+		}
+		if (Input.GetAxisRaw ("360_RightVertical")) 
+		{
+			Debug.Log("A Button2");
+		}
+
+	}
+}
+
