52f5c13e23d98e10188f730850362cdfbec29022
David Sexton <6530958533@r9tigermail.org>
2015-10-03 14:29:23 -0600

    UPDATE Jumping Changes
    -Character can now only jump twice in a row.
    -There are setting for jump height while on ground and in the air.
    -You can change the overall jump power.

--- Assets/Scripts/AJUMPFORDEVITO.cs
+++ Assets/Scripts/AJUMPFORDEVITO.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class AJUMPFORDEVITO : MonoBehaviour {
-	public float jumpHeight;
+
+	//Used to determine how high the character can jump in air and on land.
+	public static float jumpHeight;
+
+	//Set the overall jump height
+	public float jumpPower;
+
+	//Set the total jump height
+	public float jumpValue = 1.0f;
+
+	// Used to determine how fast to move in air and on land
 	public static float moveSpeed = 0f;
+
+	// Used to set overall speed
 	public static float speed = .9f;
+
+	// Final Speed Value
 	public float moveValue;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,15 +28,25 @@ public class AJUMPFORDEVITO : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		UserInputs ();
-		moveValue = Input.GetAxis("360_LeftHorizontal") * moveSpeed * speed;	
+
+		//Calculates the final speed.
+		moveValue = Input.GetAxis("360_LeftHorizontal") * moveSpeed * speed;
+
+		//Calculates the final jump height.
+		jumpValue = jumpHeight * jumpPower;
+
 	}
 	void UserInputs() {
+
+		// If Button A is Hit, then jump.
 		if (Input.GetButtonDown ("360_AButton")) {
 			Debug.Log("Pls Jump");
-			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
-	}
-	
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpValue);
+		}
+
+		// If Left Joystick is being used, then change the move speed.
 		if(Input.GetAxis("360_LeftHorizontal") != 0) {
 			GetComponent<Rigidbody2D>().AddForce(Vector2.right * moveValue);
 
--- Assets/Scripts/GroundTouch.cs
+++ Assets/Scripts/GroundTouch.cs
@@ -2,8 +2,20 @@
 using System.Collections;
 
 public class GroundTouch : MonoBehaviour {
+	//Set speed on ground
 	public float groundSpeed;
+
+	//Set speed in the air
 	public float airSpeed;
+
+	//Set jump height in air
+	public float doublejumpHeight;
+
+	//Find amount of times jumped
+	public float NumberofJumps;
+
+	//Set jump height on ground
+	public float groundjumpHeight;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,15 +23,36 @@ public class GroundTouch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (Input.GetButtonDown ("360_AButton")) {
+			
+			NumberofJumps = NumberofJumps + 1; 
+
+		}
+		//Can't jump if you have already jumped twice. I mean, it's already kind of impressive that a person can jump while in the air.
+		if (NumberofJumps == 2) {
+			AJUMPFORDEVITO.jumpHeight = 0f;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		//Makes you move at a normal speed on ground
 		AJUMPFORDEVITO.moveSpeed = groundSpeed;
+
+		//Set Jumpheight for being on the ground
+		AJUMPFORDEVITO.jumpHeight = groundjumpHeight;
+
+		//When you land, reset the number of jumps
+		NumberofJumps = 0;
 	}
 	void OnTriggerExit2D(Collider2D other) {
 		//You become slower in the air, because you are in the air.
 		AJUMPFORDEVITO.moveSpeed = airSpeed;
+
+		//If double jumping, set the jump height to double jump height.
+		if (NumberofJumps < 2) {
+			AJUMPFORDEVITO.jumpHeight = doublejumpHeight;
+		}
+
 	}
 }
