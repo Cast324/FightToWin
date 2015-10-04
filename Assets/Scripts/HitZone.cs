ebf6302470e54072189f0693009893c0e0709d9b
David Sexton <6530958533@r9tigermail.org>
2015-10-04 13:57:58 -0600

    UPDATE Punching
    -When an object with the "MoveableObject" tag is in the punching trigger
    zone, and the B button is pressed, a force will be applied to that
    object.
    - Currently the box only stays on the right side, so it needs to be
    translated to the left side when the velocity becomes negative.

---
+++ Assets/Scripts/HitZone.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitZone : MonoBehaviour {
+
+	public bool punchIt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UserInputs ();
+
+		
+	}
+	void OnTriggerStay2D(Collider2D other){
+
+		if (other.tag == "MoveableObject") {
+			Debug.Log ("INTHEPUNCHZONE");
+			if (punchIt == true){
+			other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 2000);
+				Debug.Log ("WEW");
+			}
+			}
+	}
+	
+	void OnTriggerExit2D(Collider2D other){
+
+	}
+	void UserInputs(){
+		punchIt = Input.GetButton ("360_BButton");
+	}
+
+	}
+
+
