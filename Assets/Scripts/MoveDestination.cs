b3cbb522e8c8450aa6342ed7129fc5f447565a07
Cast324 <michaelblades12@gmail.com>
2015-10-08 00:06:48 -0500

    Added basic AI/PathFinding Juming is a little odd when using AI need to redo ground detection all together it messes with AI AI can pathfind and jump up and down in the level to reach the player

---
+++ Assets/Scripts/MoveDestination.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDestination : MonoBehaviour {
+	// Getting Target object , destination of that object, and the navmeshagent
+	public Transform target;
+	Vector3 destination;
+	NavMeshAgent agent;
+	
+	void Start () {
+		// Cache agent component and destination
+		agent = GetComponent<NavMeshAgent>();
+		destination = agent.destination;
+	}
+	
+	void Update () {
+		// Update destination if the target moves one unit
+		if (Vector3.Distance (destination, target.position) > 1.0f) {
+			destination = target.position;
+			agent.destination = destination;
+		}
+	}
+
+}
